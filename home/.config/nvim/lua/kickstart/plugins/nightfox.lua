b23af465c46148b1cddfb8cc4b401cf5f099ed92
Noriyuki Abe <noriyukipy@gmail.com>
2026-02-01 19:02:34 +0900

    Set line number background color

--- home/.config/nvim/lua/kickstart/plugins/nightfox.lua
+++ home/.config/nvim/lua/kickstart/plugins/nightfox.lua
@@ -1,7 +1,17 @@
 return {
   'EdenEast/nightfox.nvim',
   config = function()
-    require('nightfox').setup({})
+    require('nightfox').setup({
+      palettes = {},
+      specs = {},
+      groups = {
+        all = {
+          -- Set background color of line number.
+          LineNR = { fg = "fg3", bg = "#122124" },  -- darker color of terafox default bg color gb1=#152528 (deep blue green)
+          -- LineNR = { bg = "#101D20" },  -- much darker color
+        },
+      }
+    })
     vim.cmd.colorscheme 'terafox'
   end,
 }
